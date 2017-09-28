0c2728f3c790cbdd202ce2c81b5e1e03e0346cc6
hao guang kok <haoguang18@hotmail.com>
2017-09-28 17:50:09 +0800

    created addItem and modify master pages

---
+++ ExpressPrintingSystem/ExpressPrintingSystem/Staff/Item/AddItem.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Staff/Staff.Master" AutoEventWireup="true" CodeBehind="AddItem.aspx.cs" Inherits="ExpressPrintingSystem.Staff.Item.AddItem" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphStaffContent" runat="server">
+    <h1>Add Item</h1>
+    <asp:Label ID="lblError" runat="server" ForeColor="Red" Text=""></asp:Label>
+    <form id="addItemForm" runat="server">
+        <table>
+            <tr>
+                <td><asp:Label ID="lblName" runat="server" Text="Name:"></asp:Label></td>
+                <td><asp:TextBox ID="txtName" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td><asp:Label ID="lblPrice" runat="server" Text="Price:"></asp:Label></td>
+                <td><asp:TextBox ID="txtPrice" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td><asp:Label ID="lblQuantity" runat="server" Text="Stock Quantity:"></asp:Label></td>
+                <td><asp:TextBox ID="txtQuantity" runat="server"></asp:TextBox></td>
+            </tr>
+            <tr>
+                <td><asp:Label ID="lblSupplier" runat="server" Text="Supplier:"></asp:Label></td>
+                <td><asp:TextBox ID="txtSupplier" runat="server"></asp:TextBox></td>
+            </tr>
+        </table>
+            <asp:Button ID="btnSubmit" runat="server" Text="Add Item" OnClick="btnSubmit_Click" />
+        
+    </form>
+</asp:Content>
---
+++ ExpressPrintingSystem/ExpressPrintingSystem/Staff/Item/AddItem.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ExpressPrintingSystem.Staff.Item
+{
+    public partial class AddItem : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            SqlConnection conPrintDB;
+            string connStr = ConfigurationManager.ConnectionStrings["printDBServer"].ConnectionString;
+            conPrintDB = new SqlConnection(connStr);
+            conPrintDB.Open();
+            try
+            {
+                string strInsert;
+                SqlCommand cmdInsert;
+
+                strInsert = "Insert Into Item (ItemName, ItemPrice, ItemStockQuantity, ItemSupplier) Values (@itemName, @itemPrice, @itemStockQuantity, @itemSupplier)";
+
+                cmdInsert = new SqlCommand(strInsert, conPrintDB);
+                cmdInsert.Parameters.AddWithValue("@itemName", txtName.Text);
+                cmdInsert.Parameters.AddWithValue("@itemPrice", Convert.ToDecimal(txtPrice.Text) );
+                cmdInsert.Parameters.AddWithValue("@itemStockQuantity", Convert.ToInt32(txtQuantity.Text));
+                cmdInsert.Parameters.AddWithValue("@itemSupplier", txtSupplier.Text);
+
+
+                int n = cmdInsert.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = "An error occured when adding item :" + ex.ToString();
+            }
+
+
+            conPrintDB.Close();
+        }
+    }
+}
