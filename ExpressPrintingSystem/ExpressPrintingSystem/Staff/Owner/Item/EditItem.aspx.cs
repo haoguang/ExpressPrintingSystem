6101af7af4a7ad3151f7aed57655ffe8cf7c897a
hao guang kok <haoguang18@hotmail.com>
2017-10-03 21:58:31 +0800

    Complete Basic CRU For Item

---
+++ ExpressPrintingSystem/ExpressPrintingSystem/Staff/Owner/Item/EditItem.aspx.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ExpressPrintingSystem.Staff.Owner.Item
+{
+    public partial class EditItem : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            populateItemToControls();
+        }
+
+        private void populateItemToControls()
+        {
+            DataTable result = null;
+            try
+            {
+                if (Request.QueryString["ItemID"] != null) { 
+                using (SqlConnection conPrintDB = new SqlConnection(ConfigurationManager.ConnectionStrings["printDBServer"].ConnectionString))
+                {
+                    string strSelect = "select ItemName, ItemPrice, ItemStockQuantity, ItemSupplier from Item where ItemID = @itemID";
+
+                    using (SqlCommand cmdSelect = new SqlCommand(strSelect, conPrintDB))
+                    {
+                        cmdSelect.Parameters.AddWithValue("@itemID", (string)Request.QueryString["ItemID"]);
+
+                        using (SqlDataAdapter da = new SqlDataAdapter(cmdSelect))
+                        {
+                            result = new DataTable();
+                            da.Fill(result);
+                        }
+
+                            
+                            lblItemID.Text = Request.QueryString["ItemID"];
+                            txtName.Text = (string)result.Rows[0]["ItemName"];
+                            txtPrice.Text = ((decimal)result.Rows[0]["ItemPrice"]).ToString();
+                            txtQuantity.Text = ((int)result.Rows[0]["ItemStockQuantity"]).ToString();
+                            txtSupplier.Text = (string)result.Rows[0]["ItemSupplier"];
+
+                        }
+
+                }
+                }
+                else
+                {
+                    lblError.Text = "Item might not exist in the database.";
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+        }
+
+        protected override void OnPreRender(EventArgs e)
+        {
+            //set default button
+            this.Form.DefaultButton = this.btnSubmit.UniqueID;
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            SqlConnection conPrintDB;
+            string connStr = ConfigurationManager.ConnectionStrings["printDBServer"].ConnectionString;
+            conPrintDB = new SqlConnection(connStr);
+            conPrintDB.Open();
+            try
+            {
+                string strInsert;
+                SqlCommand cmdInsert;
+
+                strInsert = "Update Item Set ItemName = @itemName, ItemPrice = @itemPrice, ItemStockQuantity = @itemStockQuantity, ItemSupplier = @itemSupplier where ItemID = @itemID ";
+
+                cmdInsert = new SqlCommand(strInsert, conPrintDB);
+                cmdInsert.Parameters.AddWithValue("@itemName", txtName.Text);
+                cmdInsert.Parameters.AddWithValue("@itemPrice", Convert.ToDecimal(txtPrice.Text));
+                cmdInsert.Parameters.AddWithValue("@itemStockQuantity", Convert.ToInt32(txtQuantity.Text));
+                cmdInsert.Parameters.AddWithValue("@itemSupplier", txtSupplier.Text);
+                cmdInsert.Parameters.AddWithValue("@itemID", lblItemID.Text);
+
+
+                int n = cmdInsert.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = "An error occured when adding item :" + ex.ToString();
+            }
+
+
+            conPrintDB.Close();
+        }
+    }
+}
---
+++ ExpressPrintingSystem/ExpressPrintingSystem/Staff/Owner/Item/ViewItem.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Staff/Staff.Master" AutoEventWireup="true" CodeBehind="ViewItem.aspx.cs" Inherits="ExpressPrintingSystem.Staff.Owner.Item.ViewItem" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphStaffContent" runat="server">
+    <h1>View Item</h1>
+    <p><asp:Label ID="lblSearch" runat="server" Text="Search :"></asp:Label><asp:TextBox ID="txtSearch" TextMode="Search" AutoPostBack="true" ToolTip="Press Enter after finish typing keywords" runat="server" Width="144px"></asp:TextBox></p>
+    <asp:GridView ID="gvItemList" runat="server" AllowSorting="True" AutoGenerateColumns="False" DataKeyNames="ItemID" DataSourceID="sdsItem" BorderColor="Black" BorderStyle="Solid" BorderWidth="1px" CellPadding="5" ForeColor="#333333" OnRowCommand="gvItemList_RowCommand">
+        <AlternatingRowStyle BackColor="White" />
+        <Columns>
+            <asp:BoundField DataField="ItemID" HeaderText="ItemID" ReadOnly="True" SortExpression="ItemID" />
+            <asp:BoundField DataField="ItemName" HeaderText="ItemName" SortExpression="ItemName" />
+            <asp:BoundField DataField="ItemPrice" HeaderText="ItemPrice" SortExpression="ItemPrice" />
+            <asp:BoundField DataField="ItemStockQuantity" HeaderText="ItemStockQuantity" SortExpression="ItemStockQuantity" />
+            <asp:BoundField DataField="ItemSupplier" HeaderText="ItemSupplier" SortExpression="ItemSupplier" />
+            <asp:ButtonField CommandName="Edit" HeaderText="Operation" ShowHeader="True" Text="Edit Item" />
+        </Columns>
+        <EditRowStyle BackColor="#99BBFF" Font-Bold="True" />
+        <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#EFF3FB" />
+        <SelectedRowStyle BackColor="#b3b3cc" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#F5F7FB" />
+        <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+        <SortedDescendingCellStyle BackColor="#E9EBEF" />
+        <SortedDescendingHeaderStyle BackColor="#4870BE" />
+    </asp:GridView>
+    <asp:SqlDataSource ID="sdsItem" runat="server" ConnectionString="<%$ ConnectionStrings:printDBServer %>" SelectCommand="SELECT [ItemID], [ItemName], [ItemPrice], [ItemStockQuantity], [ItemSupplier] FROM [Item] WHERE ([ItemID] LIKE '%' + @keyword + '%')  OR ([ItemName] LIKE '%' + @keyword + '%') OR ([ItemSupplier] LIKE '%' + @keyword + '%')">
+        <SelectParameters>
+            <asp:ControlParameter ControlID="txtSearch" Name="keyword" PropertyName="Text" DefaultValue="i" />
+        </SelectParameters>
+    </asp:SqlDataSource>
+</asp:Content>
