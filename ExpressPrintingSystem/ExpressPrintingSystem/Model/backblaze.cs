84000b019fd59ab68363ef8712a4f34c41e282bd
hao guang kok <haoguang18@hotmail.com>
2017-10-11 16:54:15 +0800

    edited backblaze class to authenticate more dynamiclly.

--- ExpressPrintingSystem/ExpressPrintingSystem/Model/backblaze.cs
+++ ExpressPrintingSystem/ExpressPrintingSystem/Model/backblaze.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,15 +14,16 @@ namespace ExpressPrintingSystem.Model
     public class backblaze
 
     {
-        private const string Account_ID = "82f988816cc5";
-        private const string applicationKey = "001a56f5789027dcdd25f1e6810dbaa88ca5220f5b";
+        private const string ACCOUNT_ID = "82f988816cc5";
+        private const string APPLICATION_KEY = "001a56f5789027dcdd25f1e6810dbaa88ca5220f5b";
+        private const string BUCKET_ID = "9892df591858d8b156ec0c15";
        
 
         public static string getAuthorization() {
 
             
-            String accountId = "82f988816cc5"; //B2 Cloud Storage Account ID
-            String applicationKey = "001a56f5789027dcdd25f1e6810dbaa88ca5220f5b"; //B2 Cloud Storage Application Key
+            String accountId = ACCOUNT_ID; //B2 Cloud Storage Account ID
+            String applicationKey = APPLICATION_KEY; //B2 Cloud Storage Application Key
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("https://api.backblazeb2.com/b2api/v1/b2_authorize_account");
             String credentials = Convert.ToBase64String(Encoding.UTF8.GetBytes(accountId + ":" + applicationKey));
             webRequest.Headers.Add("Authorization", "Basic " + credentials);
@@ -28,17 +31,18 @@ namespace ExpressPrintingSystem.Model
             WebResponse response = (HttpWebResponse)webRequest.GetResponse();
             String json = new StreamReader(response.GetResponseStream()).ReadToEnd();
             response.Close();
-            Console.WriteLine(json);
 
-            //String json = new StreamReader(response.GetResponseStream()).ReadToEnd();
             return json;
         }
 
         public static String getUploadUrl()
         {
-            String apiUrl = "https://api001.backblazeb2.com"; //Provided by b2_authorize_account 
-            String accountAuthorizationToken = "3_20171010032419_a9f8f62842c9abf40ac614a5_9bfae7c6fea3170634d8e43ee249f3e887323f77_001_acct";//Provided by b2_authorize_account
-            String bucketId = "9892df591858d8b156ec0c15"; //The unique bucket ID
+            string authorizationString = getAuthorization(); // get any information for authorization;
+            var authorizationObject = (JObject)JsonConvert.DeserializeObject(authorizationString);
+
+            String apiUrl = authorizationObject["apiUrl"].Value<string>(); //Provided by b2_authorize_account 
+            String accountAuthorizationToken = authorizationObject["authorizationToken"].Value<string>();//Provided by b2_authorize_account
+            String bucketId = BUCKET_ID; //The unique bucket ID
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(apiUrl + "/b2api/v1/b2_get_upload_url");
             string body = "{\"bucketId\":\"" + bucketId + "\"}";
             var data = Encoding.UTF8.GetBytes(body);
@@ -60,14 +64,13 @@ namespace ExpressPrintingSystem.Model
         }
 
 
-        public void UploadFile() {
+        public string UploadFile(string contentType, string filePath, string fileName) {
 
+            String uploadUrlInfo = getUploadUrl();//get any information to upload file.
+            var uploadUrlObject = (JObject)JsonConvert.DeserializeObject(uploadUrlInfo);
 
-            String uploadUrl = "https://pod-000-1091-10.backblaze.com/b2api/v1/b2_upload_file/9892df591858d8b156ec0c15/c001_v0001091_t0022";
-            String uploadAuthorizationToken = "3_20171010044654_c29d6f95ec2a61e8c17ad6ac_a4360d7db18de54c8f02118ba7ff9e4395890bb1_001_upld"; //Provided by b2_get_upload_url
-            String contentType = "CONTENT_TYPE"; //Type of file i.e. image/jpeg, audio/mpeg...
-            String filePath = "FILE_PATH"; //File path of desired upload 
-            String fileName = "FILE_NAME"; //Desired name for the file
+            String uploadUrl = uploadUrlObject["uploadUrl"].Value<string>();
+            String uploadAuthorizationToken = uploadUrlObject["authorizationToken"].Value<string>(); //Provided by b2_get_upload_url
             String sha1Str = "SHA_1"; //Sha1 verification for the file
 
             // Read the file into memory and take a sha1 of the data.
@@ -98,7 +101,8 @@ namespace ExpressPrintingSystem.Model
             WebResponse response = (HttpWebResponse)webRequest.GetResponse();
             var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
             response.Close();
-            Console.WriteLine(responseString);
+
+            return responseString;
         }
 
     }
