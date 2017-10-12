28f013db4d25535e529421fc4451b63b0e034c8f
lai <darrenlai95@gmail.com>
2017-10-13 00:45:58 +0800

    tml morning continue

--- ExpressPrintingSystem/ExpressPrintingSystem/booking.aspx.cs
+++ ExpressPrintingSystem/ExpressPrintingSystem/booking.aspx.cs
@@ -27,17 +27,32 @@ namespace ExpressPrintingSystem.Customer
 
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+            //if (Request.Cookies["UserCookie"] != null)
+            //{
+
+            //    var Cookie = Request.Cookies["UserCookie"];
+
+            //    if (Cookie.Values["UserInfo"] != null)
+            //    {
+            //        string userString = ClassHashing.basicDecryption(Cookie.Values["UserInfo"].ToString());
+            //        User user = ExpressPrintingSystem.Model.Entities.User.toUserObject(userString);
+            //        txtcustomerID.Text = user.Name;
+            //    }
+            //}
         }
 
-      
+
 
         protected void Button1_Click(object sender, EventArgs e)
         {
 
+
+            //try
+            //{
             if (FileUpload1.HasFile)
+
             {
-               //upload to backblaze
+                //upload to backblaze
                 String contentType = FileUpload1.PostedFile.ContentType; //Type of file i.e. image/jpeg, audio/mpeg...
                 String getPath = Path.GetFileName(FileUpload1.FileName);
                 FileUpload1.SaveAs(Server.MapPath("~/File/") + getPath);//save to server side file
@@ -48,79 +63,99 @@ namespace ExpressPrintingSystem.Customer
 
                 string getFileIDInCloud = backblaze.UploadFile(contentType, filePath, fileName);
 
+               
+
 
                 // get the page number
-               
-                 var application = new Application();
-                var document = application.Documents.Open(filePath);
-                var numberOfPages = document.ComputeStatistics(WdStatistic.wdStatisticPages, false);
                 
-
-
+                var application = new Application();
+                var document = application.Documents.Open(filePath);//open document
+               
+                var numberOfPages = document.ComputeStatistics(WdStatistic.wdStatisticPages, false);
+                document.Close();///close document
 
                 // upload to my sqldatabase
                 var uploadFileObject = (JObject)JsonConvert.DeserializeObject(getFileIDInCloud);
                 String FileIdInCloud = uploadFileObject["fileId"].Value<string>();//get file ID
-                this.UploadFileToDatabase(FileIdInCloud, numberOfPages, contentType, fileName, size);//pass to uploadfiledatabase
+
+                UploadFileToDatabase(FileIdInCloud, numberOfPages, contentType, fileName, size, filePath);//pass to uploadfiledatabase
+                
+
 
             }
-            else {
+            else
+            {
                 Response.Write("<script LANGUAGE='JavaScript' >alert('Please Upload a file First!!!')</script>");
             }
-
-           
-
         }
+        //catch (Exception ex) {
+        //    throw new Exception(ex.ToString());
+        //}
 
 
-        public void UploadFileToDatabase(string fileid, int pageNumber, string contentType, string filename, int size)
+
+    
+
+    public void UploadFileToDatabase(string fileid, int pageNumber, string contentType, string filename, int size, string path)
+    {
+
+
+
+
+        SqlConnection conTaxi;
+        string connStr = ConfigurationManager.ConnectionStrings["printDBServer"].ConnectionString;
+        conTaxi = new SqlConnection(connStr);
+        conTaxi.Open();
+
+        string strInsert;
+        SqlCommand cmdInsert;
+
+
+        strInsert = "Insert Into Document (DocumentName, DocumentType, FileIDInCloud, CustomerID, Size, PageNumber) Values (@DocumentName, @DocumentType, @FileIDInCloud, @CustomerID, @Size, @PageNumber)";
+        cmdInsert = new SqlCommand(strInsert, conTaxi);
+
+        if (FileUpload1 != null)
         {
 
 
-           
+            cmdInsert.Parameters.AddWithValue("@DocumentName", filename);
+            cmdInsert.Parameters.AddWithValue("@DocumentType", contentType);
+            cmdInsert.Parameters.AddWithValue("@FileIDInCloud", fileid);
+            cmdInsert.Parameters.AddWithValue("@CustomerID", CustomerID);
+            cmdInsert.Parameters.AddWithValue("@Size", size);
+            cmdInsert.Parameters.AddWithValue("@PageNumber", pageNumber);
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
 
-            SqlConnection conTaxi;
-            string connStr = ConfigurationManager.ConnectionStrings["printDBServer"].ConnectionString;
-            conTaxi = new SqlConnection(connStr);
-            conTaxi.Open();
-
-            string strInsert;
-            SqlCommand cmdInsert;
-
-
-            strInsert = "Insert Into Document (DocumentName, DocumentType, FileIDInCloud, CustomerID, Size, PageNumber) Values (@DocumentName, @DocumentType, @FileIDInCloud, @CustomerID, @Size, @PageNumber)";
-            cmdInsert = new SqlCommand(strInsert, conTaxi);
-
-            if (FileUpload1 != null)
-            {
-             
-
-                cmdInsert.Parameters.AddWithValue("@DocumentName", filename);
-                cmdInsert.Parameters.AddWithValue("@DocumentType", contentType);
-                cmdInsert.Parameters.AddWithValue("@FileIDInCloud", fileid);
-                cmdInsert.Parameters.AddWithValue("@CustomerID", CustomerID);
-                cmdInsert.Parameters.AddWithValue("@Size", size);
-                cmdInsert.Parameters.AddWithValue("@PageNumber", pageNumber);
-
+                }
 
             }
 
+
             int n = cmdInsert.ExecuteNonQuery();
 
-            if (n > 0)
-                Response.Write("<script>alert('Upload Successful');</script>");
-            else
-                Response.Write("<script>alert('Upload Failed');</script>");
+        if (n > 0)
+        {
+            Response.Write("<script>alert('Upload Successful');</script>");
+             
+            Response.Redirect("~/Customer/Payment.aspx");
+        }
+        else
+        {
+            Response.Write("<script>alert('Upload Failed');</script>");
+        }
 
             /*Close database connection*/
 
+
             conTaxi.Close();
-            Response.Redirect("Payment.aspx");
+
         }
-
-
-
-
-
-    }
 }
+}
+
+
+
+
+
